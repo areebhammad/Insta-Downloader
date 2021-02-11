90d5a3a78580d0d443653ea3546f9571d6ad2658
areebhammad <48858704+areebhammad@users.noreply.github.com>
2021-02-11 19:34:16 +0530

    Update script.js

--- Insta-Media-Downloader-master/script.js
+++ Insta-Media-Downloader-master/script.js
@@ -1,1 +1,51 @@
-$("#getMedia").click(getMedia);var render=document.querySelector("#media");function createMedia(e,t){var r=document.createElement(t);r.id="instagramMedia",r.src=e.content,r.setAttribute("class",""),r.width=$(render).width(),"video"===t&&(r.controls=!0,r.autoplay=!0),render.innerHTML="";var a=document.createElement("p");a.setAttribute("class","bg-success text-info"),a.innerText="The Media is secured ✅\n Right Click on the Media below to get Save Option!",render.appendChild(a),render.appendChild(r)}function getMedia(){var e=$("#postUrl").val();e?$.get(e,function(e){render.innerHTML=e;var t=setTimeout(function(){var e=document.querySelector('meta[property="og:video"]');if(e)createMedia(e,"video");else{var r=document.querySelector('meta[property="og:image"]');r?createMedia(r,"img"):(document.body.innerHTML=body,alert("Error extracting Instagram image / video."))}clearTimeout(t)},200)}):document.querySelector("#media").setAttribute("placeholder","Invalid Address, Please Enter Proper Insagram Link")}
+$("#getMedia").click(getMedia);
+var render = document.querySelector("#media");
+
+function createMedia(data, type) {
+	var media = document.createElement(type);
+	media.id = "instagramMedia";
+	media.src = data.content;
+	media.setAttribute("class", "");
+	media.width = $(render).width();
+	
+	if (type === "video") {
+		media.controls = true;
+		media.autoplay = true;		
+	} 
+	render.innerHTML = "";
+	var downloadMsg = document.createElement("p");
+	downloadMsg.setAttribute("class", "bg-success text-info");
+	downloadMsg.innerText = "Right Click on the Media below to get Save Option!";
+
+	render.appendChild(downloadMsg);
+	render.appendChild(media);
+}
+
+function getMedia() {
+	var url = $("#postUrl").val();
+	if (url) {
+		$.get(url, function (data) {
+			render.innerHTML = data;
+			var mediaWaitTimer = setTimeout(function () {
+				var video = document.querySelector('meta[property="og:video"]');
+				if (video) {
+					createMedia(video, "video");
+				} else {
+					var img = document.querySelector('meta[property="og:image"]');
+					if (img) {
+						createMedia(img, "img");
+					} else {
+						document.body.innerHTML = body;
+						alert("Error extracting Instagram image / video.");
+					};
+				}
+				clearTimeout(mediaWaitTimer);
+			}, 200);
+
+		});
+
+	} else {
+		document.querySelector("#media").setAttribute("placeholder", "Invalid Address, Please Enter Proper Insagram Link");
+
+	}
+}
